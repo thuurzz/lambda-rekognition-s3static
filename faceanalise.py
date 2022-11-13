b47e1e4c6ff2e3cbc3067c350e067edd66011dae
Arthur Vinicius <thuur.vss@gmail.com>
2022-11-13 00:47:55 -0300

    07. Finalizando o código

--- faceanalise.py
+++ faceanalise.py
@@ -54,8 +54,22 @@ def gera_dados_json(resultado_comparacao):
   return dados_json
    
 
+def publica_dados(dados_json):
+  bucket = s3.Object("lambda-site-alura", "dados.json")
+  bucket.put(Body=json.dumps(dados_json))
+
+
+def exclui_imagem_colecao(faceId_detectadas):
+  client.delete_faces(
+    CollectionId='faces',
+        FaceId=faceId_detectadas,
+  )
+
+#================== MAIN
 faces_detectadas = detecta_faces()
 faceId_detectadas = cria_lista_faceId_detectadas(faces_detectadas)
 resultado_comparacao = compara_imagens(faceId_detectadas)
 dados_json = gera_dados_json(resultado_comparacao)
+publica_dados(dados_json)
+exclui_imagem_colecao(faceId_detectadas)
 print(json.dumps(dados_json, indent=4))
